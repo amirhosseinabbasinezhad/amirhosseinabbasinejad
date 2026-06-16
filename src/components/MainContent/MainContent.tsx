28d79982e3aff663cbc8834dca05975e932d0c8c
AmirhosseinAbbasinezhad <amirabbasinezhad6@gmail.com>
2026-06-17 00:14:40 +0330

    9

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider } from '@mui/material';
+import { Box } from '@mui/material';
 import { About } from './About';
 import { Experience } from './Experience';
 import { Projects } from './Projects';
